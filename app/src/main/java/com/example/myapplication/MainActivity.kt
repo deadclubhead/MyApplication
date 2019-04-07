22267498262937e07560750c150f16faf5d2495b
dch <deadclubhead@gmail.com>
2019-04-07 18:21:11 +0300

    hellow world New func

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,5 +8,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val result = myFirstFunction(2, 3.2)
+        println(result)
+
     }
 }
+fun myFirstFunction(i: Int , l : Double) = i / l
